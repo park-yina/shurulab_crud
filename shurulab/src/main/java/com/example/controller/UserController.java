d1e29fcb0f6d7b06d191e590ad09e3c8cf63c995
park-yina <111878820+park-yina@users.noreply.github.com>
2024-06-14 20:59:20 +0900

    Update UserController.java

--- shurulab/src/main/java/com/example/controller/UserController.java
+++ shurulab/src/main/java/com/example/controller/UserController.java
@@ -66,6 +66,12 @@ public class UserController {
             return "login"; // 로그인 실패 시 로그인 페이지로 이동
         }
     }
+        @GetMapping("/user/logout")
+    public String logout(HttpSession session) {
+        session.invalidate(); // 로그아웃 시 세션 무효화
+        return "redirect:/user/login?logout=true";
+    }
+
     @GetMapping("/info")
     public String getInfo(HttpSession session, Model model) throws IOException {
         Object username = session.getAttribute("username");
